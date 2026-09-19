9389e742af134121c0c5cc7fc30223b8f7ec61b3
hilidoors <hilidoors@gmail.com>
2026-09-19 11:52:42 -0400

    Fetch fresh shell when updating offline cache

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='prague-joy-v2';
+const CACHE='prague-joy-v3';
 const SHELL=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(async c=>{await Promise.all(SHELL.map(async url=>{const res=await fetch(new Request(url,{cache:'reload'}));if(!res.ok)throw new Error('Failed to cache '+url);await c.put(url,res)}))}).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
